8187fa30b92aec03fa7d490bea03c5270d1657db
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-04-01 22:50:39 +0530

    carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Menu from './components/Menu';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Slider from './components/Carousel';
 function App() {
   return (
     <div className="App">
       <Menu />
+      <Slider />
     </div>
   );
 }
