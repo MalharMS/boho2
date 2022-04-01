72939c55f54cf31614cacadca1d259e0d0e49737
aryanjangid <aryanjangid2511@gmail.com>
2022-04-01 22:50:09 +0530

    assests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Menu from './components/Menu';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Brands from './components/Brands';
 function App() {
   return (
     <div className="App">
       <Menu />
+      <Brands />
     </div>
   );
 }
