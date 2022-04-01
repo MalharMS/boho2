9784e01203ff4da914fa677b328c9ef45f8f33f4
aryanjangid <aryanjangid2511@gmail.com>
2022-04-01 23:09:32 +0530

    brands done

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import './brands.css'
-import img1 from '../Assests/image3.jpg'
+import img1 from '../Assests/image 4.png'
 function Brands() {
     return (
         <div className='brands-main'>
-            <div>
-                <div className='brand-indi'><img src={img1} alt='brandimage'></img></div>
-                <div className='brand-indi'><img src={img1} alt='brandimage'></img></div>
-                <div className='brand-indi'><img src={img1} alt='brandimage'></img></div>
-            </div>
-            <div>
-                <div className='brand-indi'><img src={img1} alt='brandimage'></img></div>
-                <div className='brand-indi'><img src={img1} alt='brandimage'></img></div>
-                <div className='brand-indi'><img src={img1} alt='brandimage'></img></div>
+            <div className='brand-container'>
+                <div className='brand-indi'><img className='brand-img' src={img1} alt='brandimage'></img></div>
+                <div className='brand-indi'><img className='brand-img' src={img1} alt='brandimage'></img></div>
+                <div className='brand-indi'><img className='brand-img' src={img1} alt='brandimage'></img></div>
+                <div className='brand-indi'><img className='brand-img' src={img1} alt='brandimage'></img></div>
+                <div className='brand-indi'><img className='brand-img' src={img1} alt='brandimage'></img></div>
+                <div className='brand-indi'><img className='brand-img' src={img1} alt='brandimage'></img></div>
+                <div className='brand-indi'><img className='brand-img' src={img1} alt='brandimage'></img></div> 
             </div>
         </div>
     )
